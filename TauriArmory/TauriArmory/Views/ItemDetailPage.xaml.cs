4d02279e14c33c23ad1fc57f511d3b694f7ba68b
Stanley1551 <peter.pal.lorincz@gmail.com>
2017-08-02 20:51:33 +0200

    Add project files.

---
+++ TauriArmory/TauriArmory/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using TauriArmory.ViewModels;
+
+using Xamarin.Forms;
+
+namespace TauriArmory.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+		{
+			InitializeComponent();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+	}
+}
